ab190bf17efa51e08c1827bb0be648607b25e5cb
Fuji, Goro <gfuji@cpan.org>
2011-12-01 23:47:49 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b7c4904..2a6c639 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Alien::RRDtool
    
    +0.02 2011-12-01 23:43:29
    +    - Install rrd* commands to perl's bin/
    +
     0.01_04 2011-12-01 01:20:59
         - Re-packaging

--- lib/Alien/RRDtool.pm
+++ lib/Alien/RRDtool.pm
@@ -3,7 +3,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.01_04';
+our $VERSION = '0.02';
 
 use File::ShareDir qw(dist_dir);;
 use File::Spec;
@@ -31,7 +31,7 @@ Alien::RRDtool - Installation of Perl bindings to RRDtool
 
 =head1 VERSION
 
-This document describes Alien::RRDtool version 0.01_04.
+This document describes Alien::RRDtool version 0.02.
 
 =head1 SYNOPSIS
 
