f9ed4fb2bec6ed11f82b0aa207737405c9aba868
Fuji, Goro <g.psy.va@gmail.com>
2013-03-31 23:29:10 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 50998a2..e705032 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,9 +1,12 @@
     Revision history for Perl extension Alien::RRDtool
    
    +0.05 2013-03-31 23:26:59
    +    - Do not override PKG_CONFIG_PATH for one to set it (thanks to kazeburo)
    +
     0.04 2013-03-30 23:51:44
         - Apply patch to work RRDtool 1.4.7 on MacOSX 10.8 (kazeburo)
           see https://github.com/oetiker/rrdtool-1.x/issues/374
    -    - set PKGCONFIG_PATH for homebrew-ed pkg-config(1)
    +    - set PKG_CONFIG_PATH for homebrew-ed pkg-config(1)
    
     0.03 2012-05-17 14:39:23
         - Ship with RRDTool 1.4.7

--- lib/Alien/RRDtool.pm
+++ lib/Alien/RRDtool.pm
@@ -3,7 +3,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use File::ShareDir qw(dist_dir);;
 use File::Spec;
@@ -31,7 +31,7 @@ Alien::RRDtool - Installation of Perl bindings to RRDtool
 
 =head1 VERSION
 
-This document describes Alien::RRDtool version 0.04.
+This document describes Alien::RRDtool version 0.05.
 
 =head1 SYNOPSIS
 
